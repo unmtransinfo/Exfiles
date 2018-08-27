2efc49b3cdf28b2a18fff5a78d17fd2df2fdd72f
Jeremy J Yang <jeremyjyang@gmail.com>
2018-08-27 16:01:40 -0600

    NxN or filtered by threshold.

---
+++ Go_exfiles_similarity.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#
+cwd=$(pwd)
+#
+DATADIR=$cwd/data
+#
+IFILE="$DATADIR/gtex_rnaseq_prep_profiles.tsv"
+#
+OFILE_TANIMOTO="$DATADIR/gtex_rnaseq_profiles_Tanimoto.tsv"
+TANIMOTO_MIN="0.5"
+OFILE_PEARSON="$DATADIR/gtex_rnaseq_profiles_Pearson.tsv"
+PEARSON_MIN="0.5"
+OFILE_SPEARMAN="$DATADIR/gtex_rnaseq_profiles_Spearman.tsv"
+SPEARMAN_MIN="0.5"
+#
+python/exfiles_similarity.py --i $IFILE \
+	--tanimoto_min $TANIMOTO_MIN --o_tanimoto $OFILE_TANIMOTO
+#
+python/exfiles_similarity.py --i $IFILE \
+	--pearson_min $PEARSON_MIN --o_pearson $OFILE_PEARSON
+#
+python/exfiles_similarity.py --i $IFILE \
+	--spearman_min $SPEARMAN_MIN --o_spearman $OFILE_SPEARMAN
+#
