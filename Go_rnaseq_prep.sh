1d87c20251f5d3a18cb8ba8d64334565d589466a
Ubuntu <ubuntu@ip-172-31-29-59.us-west-1.compute.internal>
2018-09-12 00:28:08 +0000

    update

--- Go_rnaseq_prep.sh
+++ Go_rnaseq_prep.sh
@@ -1,15 +1,16 @@
 #!/bin/sh
 
-SRCDATADIR="/home/ubuntu/data"
-rnaseqfile="$SRCDATADIR/GTEx_Analysis_2016-01-15_v7_RNASeQCv1.1.8_gene_tpm_demo.gct.gz"
+SRCDATADIR="/renci/irods/gtex"
+OUTPUT_DIR="${HOME}/expression-profiles/data/output"
+mkdir ${OUTPUT_DIR}
 
 ./python/gtex_rnaseq_prep_app.py \
-	--i_subject $SRCDATADIR/GTEx_v7_Annotations_SubjectPhenotypesDS.txt \
-	--i_sample $SRCDATADIR/GTEx_v7_Annotations_SampleAttributesDS.txt \
+	--i_subject $SRCDATADIR/Annotations/GTEx_v7_Annotations_SubjectPhenotypesDS.txt \
+	--i_sample $SRCDATADIR/Annotations/GTEx_v7_Annotations_SampleAttributesDS.txt \
 	--i_gene $SRCDATADIR/gtex_gene_xref.tsv \
-	--i_rnaseq $rnaseqfile \
-	--o_sample $SRCDATADIR/gtex_rnaseq_prep_sample.tsv \
-	--o_median $SRCDATADIR/gtex_rnaseq_prep_median.tsv \
-	--o_profiles $SRCDATADIR/gtex_rnaseq_prep_profiles.tsv \
-	--o_tissue $SRCDATADIR/gtex_rnaseq_prep_tissues.tsv \
+	--i_rnaseq ${SRCDATADIR}/RNA-seq/GTEx_Analysis_2016-01-15_v7_RNASeQCv1.1.8_gene_tpm_demo.gct.gz \
+	--o_sample $SRCDATADIR/output/gtex_rnaseq_prep_sample.tsv \
+	--o_median $SRCDATADIR/output/gtex_rnaseq_prep_median.tsv \
+	--o_profiles $SRCDATADIR/output/gtex_rnaseq_prep_profiles.tsv \
+	--o_tissue $SRCDATADIR/output/gtex_rnaseq_prep_tissues.tsv \
 	-v
