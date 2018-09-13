0d652645683d207147155d9ce73e26effaeb6f99
Jeremy J Yang <jeremyjyang@gmail.com>
2018-09-13 08:01:29 -0600

    MP output file concatenation.

--- Go_exfiles_similarity.sh
+++ Go_exfiles_similarity.sh
@@ -11,29 +11,33 @@ DATADIR="$cwd/data"
 IFILE="$DATADIR/gtex_rnaseq_prep_profiles.tsv"
 N_IDCOLS="2"
 #
+###
 #OFILE_TANIMOTO="$DATADIR/gtex_rnaseq_profiles_Tanimoto.tsv"
 #TANIMOTO_MIN="0"
+#
+#python/exfiles_similarity.py --i $IFILE \
+#	--tanimoto_min $TANIMOTO_MIN --o_tanimoto $OFILE_TANIMOTO
+###
 #
 OFILE_RUZICKA="$DATADIR/gtex_rnaseq_profiles_Ruzicka.tsv"
 RUZICKA_MIN="0"
 #
-#OFILE_PEARSON="$DATADIR/gtex_rnaseq_profiles_Pearson.tsv"
-#
-OFILE_SPEARMAN="$DATADIR/gtex_rnaseq_profiles_Spearman.tsv"
-#
-#python/exfiles_similarity.py --i $IFILE \
-#	--o_pearson $OFILE_PEARSON
-#
+#TOO SLOW!
 #python/exfiles_similarity.py --i $IFILE --n_idcols "$N_IDCOLS" \
-#	--o_spearman $OFILE_SPEARMAN
-#
-#python/exfiles_similarity.py --i $IFILE \
-#	--tanimoto_min $TANIMOTO_MIN --o_tanimoto $OFILE_TANIMOTO
-#
-python/exfiles_similarity.py --i $IFILE --n_idcols "$N_IDCOLS" \
+#	--ruzicka_min $RUZICKA_MIN --o_ruzicka $OFILE_RUZICKA
+###
+N_PROC="8"
+rm -f ${OFILE_RUZICKA}_*
+python/exfiles_ruzickaMP.py --i $IFILE --n_idcols "$N_IDCOLS" --nproc $N_PROC \
 	--ruzicka_min $RUZICKA_MIN --o_ruzicka $OFILE_RUZICKA
+head -1 ${OFILE_RUZICKA}_01 >$OFILE_RUZICKA
+for f in $(ls ${OFILE_RUZICKA}_*) ; do
+	cat $f |sed -e '1d' >>$OFILE_RUZICKA
+done
+###
 #
 OFILE_WPEARSON="$DATADIR/gtex_rnaseq_profiles_WPearson.tsv"
 #
 $(cwd)/R/exfiles_similarity.R $IFILE $OFILE_WPEARSON
 #
+###
