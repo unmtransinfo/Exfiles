edc6fcf5889ec80656cd982dc9440bdbfc993a1e
Ubuntu <ubuntu@ip-172-31-29-59.us-west-1.compute.internal>
2018-09-17 00:12:11 +0000

    updated

--- irods_start.sh
+++ irods_start.sh
@@ -22,15 +22,13 @@ fi
 
 # Add additional setup to Irods container in order to run UNM notebook
 echo 'Creating directories and installing dependencies...'
-docker exec -ti ${CONTAINER_NAME} sudo /home/dockeruser/venv/bin/pip3 install --upgrade pip
-docker exec -ti ${CONTAINER_NAME} sudo  /home/dockeruser/venv/bin/pip3 install scipy matplotlib
-docker cp /home/ubuntu/data/GTEx_Analysis_2016-01-15_v7_RNASeQCv1.1.8_gene_tpm_demo.gct.gz ${CONTAINER_NAME}:"${GTEX_IRODS_DIR}/RNA-seq/gtex/"
+# docker exec ${CONTAINER_NAME} sudo /home/dockeruser/venv/bin/pip3 install --upgrade pip
+docker exec -ti ${CONTAINER_NAME} sudo pip3 install pandas numpy scipy matplotlib
+docker cp /home/ubuntu/data/GTEx_Analysis_2016-01-15_v7_RNASeQCv1.1.8_gene_tpm_demo.gct.gz ${CONTAINER_NAME}:"${DOCKER_DIR}"
 
-docker exec -ti ${CONTAINER_NAME} git clone https://github.com/unmtransinfo/expression-profiles.git &&
-       sh -c "cd '${HOME}/expression-profiles'" &&
-       sh -c "echo 'CURRENT DIRECTORY IS: $(pwd)'" &&
-       sh -c "git checkout containerize" &&
-       sh -c "./Go_rnaseq_prep.sh"
+docker exec ${CONTAINER_NAME} git clone https://github.com/unmtransinfo/expression-profiles.git 
+docker exec ${CONTAINER_NAME} sh -c "cd /home/dockeruser/expression-profiles && git checkout containerize"
+docker exec -ti ${CONTAINER_NAME} sh -c "cd /home/dockeruser/expression-profiles && ./Go_rnaseq_prep.sh"
 
 
 
