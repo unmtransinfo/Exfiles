192480c066254cf9a99125067bcb128ac9c93d5e
Jeremy J Yang <jeremyjyang@gmail.com>
2018-10-02 15:27:20 -0600

    IDG integration. Tooltips. Icons.

--- R/exfiles/app.R
+++ R/exfiles/app.R
@@ -38,6 +38,10 @@ if (file.exists("exfiles.Rdata")) {
   ###
   gene <- read_delim("gtex_gene_xref.tsv", "\t") # gene attributes
   ###
+  idg <- read_delim("gtex_gene_idg.tsv", "\t") # IDG gene/protein attributes
+  idg <- idg[,c("accession", "idgTDL", "idgFamily")]
+  colnames(idg) <- c("uniprot", "idgTDL", "idgFamily")
+  ###
   # ENSG, SEX, tissue.1, tissue.2, etc.
   ###
   eps <- read_delim("exfiles_eps.tsv", "\t", col_types=cols(SEX=col_character())) # expression profiles
@@ -46,7 +50,7 @@ if (file.exists("exfiles.Rdata")) {
   ###
   ggc <- read_delim("exfiles_ggc.tsv.gz", "\t", col_types="cccdd")
   ###
-  save(tissue, gene, eps, ggc, file="exfiles.Rdata")
+  save(tissue, gene, idg, eps, ggc, file="exfiles.Rdata")
 }
 #
 tissue_missing <- setdiff(tissue$SMTSD, colnames(eps))
@@ -82,8 +86,13 @@ message(sprintf("Duplicated/ambiguous gene SYMBs: %d", length(symb_dups)))
 gene <- gene[!duplicated(gene$ENSG),]
 gene <- gene[!duplicated(gene$symbol),]
 #
+message(sprintf("Gene count: %d", nrow(gene)))
 message(sprintf("Gene unique ENSG count: %d", length(unique(gene$ENSG))))
 message(sprintf("Gene unique SYMB count: %d", length(unique(gene$symbol))))
+#
+gene <- merge(gene, idg, by="uniprot", all.x=T, all.y=F)
+message(sprintf("Genes mapped to IDG: %d", sum(!is.na(gene$uniprot))))
+#
 ###
 ggc$Combo <- round(ggc$wRho*ggc$Ruzicka, digits=2)
 #
@@ -152,12 +161,11 @@ correlated or anti-correlated profiles.</B>
 <LI>Groups denote comparisons among F-only, M-only or F vs M.
 <LI>To find anti-correlated genes, search by wRho and reverse-sort results table.
 <LI><B>Sex-linked genes</B> may be identified via chromosomal location (X*/Y*).
+<LI><B>IDG (Illuminating the Druggable Genome)</B> provides target development level (TDL) and links to IDG resources for drug discovery applications.
 </UL></P>
 <P>
 Notes on data preparation: This version is focused on SABV knowledge discovery, thus reproductive and 
-breast tissues not considered. Also we restrict to protein-encoding genes mapped to HUGO gene symbols,
-for scientific comprehensibility, so only data associated with these genes is retained.
-Currently also genes are ignored with mapping ambiguity between Ensembl ENSG to HUGO symbols. 
+breast tissues not considered. Also we restrict to protein-encoding genes unambiguously mapped to HUGO gene symbols.
 </P>
 <B>Algorithms:</B> Giovanni Bocci, Oleg Ursu, Cristian Bologa, Steve Mathias, Jeremy Yang &amp; Tudor Oprea<BR/>
 <B>Web app:</B> Jeremy Yang<BR/>
@@ -174,24 +182,21 @@ ui <- fluidPage(
   fluidRow(
     column(4, 
         wellPanel(
-          actionButton("randGene", "GeneA (click for random)", style='padding:4px; background-color:#DDDDDD; font-size:100%; font-weight:bold'),
-          selectizeInput("qryA", label=NULL, choices = gene_choices, selected=qryArand),
+          selectizeInput("qryA", label="GeneA", choices = gene_choices, selected=qryArand),
           selectizeInput("qryB", label="GeneB (optional)", choices = c(list('None'='none'), gene_choices)),
-          radioButtons("mode", "Mode", choices=c("View", "Search", "Compare"), selected="View", inline=T),
+          radioButtons("mode", "Mode", choices=c("View", "Compare", "Search"), selected="View", inline=T),
           #checkboxInput("SABV", span("SABV", icon("venus",lib="font-awesome"),icon("mars", lib="font-awesome")), value=T),
           radioButtons("score", "Score", choices=c("Ruzicka", "wRho", "Combo"), selected="Combo", inline=T),
           checkboxGroupInput("searchgroups", "Searchgroups", choices=c("F","M","FM"), selected=c("F","M","FM"), inline=T),
-          checkboxGroupInput("plot_opts", "Plot",
-		choices=c("LogY"="LogY", "+Stats"="Showstats", "+SABV"="SABV"),
-		selected=c("LogY","SABV"), inline=T),
+          checkboxGroupInput("opts", "Output", choices=c("SABV","IDG","LogY","Annplot"), selected=c("SABV","IDG","LogY"), inline=T),
           br(),
+          actionButton("randGene", "Demo", style='padding:4px; background-color:#DDDDDD; font-weight:bold'),
           actionButton("goRefresh", "Refresh", style='padding:4px; background-color:#DDDDDD; font-weight:bold'),
           actionButton("showhelp", "Help", style='padding:4px; background-color:#DDDDDD; font-weight:bold')
           )),
     column(8, conditionalPanel(condition="true", plotlyOutput("plot", height = "580px")))
   ),
-  # How to test in JS for R NULL?
-  conditionalPanel(condition="(input.mode=='Search' && typeof output.datarows !== 'undefined')",
+  conditionalPanel(condition="(input.mode=='Search' && typeof output.datarows !== 'undefined')", # JS test for R NULL?
     wellPanel(
       fluidRow(column(12, DT::dataTableOutput("datarows"))),
       fluidRow(column(12, downloadButton("hits_file", label="Download")))),
@@ -202,12 +207,26 @@ ui <- fluidPage(
       htmlOutput(outputId = "log_htm", height = "60px")))),
   fluidRow(
     column(12, em(strong(sprintf("%s", APPNAME)), " web app from ", 
-	tags$a(href="http://datascience.unm.edu", span("UNM", tags$img(id="unm_logo", height="60", valign="bottom", src="unm_new.png")),
+	tags$a(href="http://datascience.unm.edu", target="_blank", span("UNM", tags$img(id="unm_logo", height="60", valign="bottom", src="unm_new.png"))),
 	" and ",
-	tags$a(href="https://nihdatacommons.us/", span("DCPPC", tags$img(id="dcppc_logo", height="60", valign="bottom", src="dcppc_logo_only.png"))
-	))))),
+	tags$a(href="https://nihdatacommons.us", target="_blank", span("DCPPC", tags$img(id="dcppc_logo", height="60", valign="bottom", src="dcppc_logo_only.png"))),
+	" data from ",
+	tags$a(href="https://gtexportal.org", target="_blank", span("GTEx", tags$img(id="gtex_logo", height="50", valign="bottom", src="GTEx_logo_only.png"))),
+	" links to ",
+	tags$a(href="https://druggablegenome.net", target="_blank", span("IDG", tags$img(id="idg_logo", height="60", valign="bottom", src="IDG_logo_only.png")))
+	))),
+  bsTooltip("qryA", "Needed for all modes.", "top"),
+  bsTooltip("qryB", "Needed for Compare, ignored for View and Search modes.", "top"),
+  bsTooltip("mode", "View 1 gene, Compare 2 genes, or Search for similar genes.", "top"),
+  bsTooltip("score", "Ruzicka similarity, Pearson weighted correlation, or combination of both.", "top"),
+  bsTooltip("searchgroups", "Search F-only, M-only, or F vs M comparisons.", "top"),
+  bsTooltip("opts", "Output options affecting plot and datatable but not query logic.", "top"),
+  bsTooltip("randGene", "Random GeneA query.", "top"),
+  bsTooltip("goRefresh", "Refresh plot, output.", "top"),
   bsTooltip("unm_logo", "UNM Translational Informatics Division", "right"),
-  bsTooltip("dcppc_logo", "NIH Data Commons Pilot Phase Consortium", "right")
+  bsTooltip("dcppc_logo", "NIH Data Commons Pilot Phase Consortium", "right"),
+  bsTooltip("gtex_logo", "GTEx, Genotype-Tissue Expression project", "right"),
+  bsTooltip("idg_logo", "IDG, Illuminating the Druggable Genome project", "right")
 )
 
 #############################################################################
@@ -277,8 +296,10 @@ server <- function(input, output, session) {
     ggc_hits["EnsemblID"] <- NA #Populate with non-query gene.
     ggc_hits$EnsemblID[ggc_hits$ENSGA==ensgA()] <- ggc_hits$ENSGB[ggc_hits$ENSGA==ensgA()]
     ggc_hits$EnsemblID[ggc_hits$ENSGB==ensgA()] <- ggc_hits$ENSGA[ggc_hits$ENSGB==ensgA()]
-    ggc_hits <- merge(ggc_hits, gene[,c("ENSG","symbol","name","chr")], by.x="EnsemblID", by.y="ENSG", all.x=T, all.y=F)
-    ggc_hits <- ggc_hits[,c("EnsemblID","symbol","name","chr","Cluster","Score")] #Must match datatable() and hits_export.
+    gene_cols <- c("ENSG", "uniprot", "symbol","name","chr","idgTDL")
+    ggc_hits <- merge(ggc_hits, gene[,gene_cols], by.x="EnsemblID", by.y="ENSG", all.x=T, all.y=F)
+    hits_cols <- c("EnsemblID","uniprot","symbol","name","chr","idgTDL","Cluster","Score") #datatable() ref by # (0+)
+    ggc_hits <- ggc_hits[,hits_cols]
     ggc_hits <- ggc_hits[order(-ggc_hits$Score),]
     return(ggc_hits)
   })
@@ -307,15 +328,19 @@ server <- function(input, output, session) {
   output$result_htm <- reactive({
     htm <- sprintf("<B>Results (%s):</B>", input$mode)
     if (!is.null(qryA())) {
-      htm <- paste0(htm, sprintf(" GeneA: %s \"%s\"", qryA(), gene$name[gene$ENSG==ensgA()]))
+      htm <- paste0(htm, sprintf(" GeneA: %s \"%s\"%s", qryA(), gene$name[gene$ENSG==ensgA()],
+	ifelse(("IDG" %in% input$opts), sprintf(" <A HREF=\"https://pharos.nih.gov/idg/targets/%s\" target=\"_blank\">%s</A>", gene$uniprot[gene$ENSG==ensgA()], icon("external-link",lib="font-awesome")), "")))
       if (!is.null(chrA()) & grepl("^[XY]", chrA())) { htm <- paste0(htm, sprintf(", sex-linked, location %s", chrA())) }
     }
     if (!is.null(qryB())) {
-      htm <- paste0(htm, sprintf("; geneB: %s \"%s\"", qryB(), gene$name[gene$ENSG==ensgB()]))
+      htm <- paste0(htm, sprintf("; geneB: %s \"%s\"%s", qryB(), gene$name[gene$ENSG==ensgB()],
+	ifelse(("IDG" %in% input$opts), sprintf(" <A HREF=\"https://pharos.nih.gov/idg/targets/%s\" target=\"_blank\">%s</A>", gene$uniprot[gene$ENSG==ensgB()], icon("external-link",lib="font-awesome")), "")))
       if (!is.null(chrB()) & grepl("^[XY]", chrB())) { htm <- paste0(htm, sprintf(", sex-linked, location %s", chrB())) }
     }
     if (!is.null(hit())) {
-      htm <- paste0(htm, sprintf("; found: %d ; top hit: %s \"%s\"; score: %s=%.2f", nrow(hits()), hit_symbol(), gene$name[gene$ENSG==hit()], input$score, hits()$Score[1]))
+      htm <- paste0(htm, sprintf("; found: %d ; top hit: %s \"%s\"%s; score: %s=%.2f", nrow(hits()), hit_symbol(), gene$name[gene$ENSG==hit()],
+	ifelse(("IDG" %in% input$opts), sprintf(" <A HREF=\"https://pharos.nih.gov/idg/targets/%s\" target=\"_blank\">%s</A>", gene$uniprot[gene$ENSG==hit()], icon("external-link",lib="font-awesome")), ""),
+	input$score, hits()$Score[1]))
       if (!is.null(hit_chr()) & grepl("^[XY]", hit_chr())) { htm <- paste0(htm, sprintf(", sex-linked, location %s", hit_chr())) }
     }
     return(htm)
@@ -327,31 +352,43 @@ server <- function(input, output, session) {
   })
 
   ### Assigns input$datarows_rows_selected
+  ### Hide ENSG, Uniprot columns.
+  ### Note col #s start with 0.
   output$datarows <- renderDataTable({
     if (is.null(hits())) { return(NULL) }
+    if ("IDG" %in% input$opts) {
+      invis_cols <- c(0,1)
+    } else {
+      invis_cols <- c(0,1,5)
+    }
     DT::datatable(data=hits(), rownames=F, 
         selection=list(target="row", mode="multiple", selected=c(1)),
 	      class="cell-border stripe", style="bootstrap",
-	      options=list(autoWidth=T, columnDefs = list(list(className='dt-center', targets=c(1,3,4,5)))), 
-	      colnames=c("Ensembl", "Symbol", "Name", "Chr", "Group", input$score)) %>%
-        formatRound(digits=2, columns=6:ncol(hits()))
+	      options=list(
+		autoWidth=T,
+		columnDefs = list(
+			list(className='dt-center', targets=c(2,4,5,6,7)),
+			list(visible=F, targets=invis_cols)
+			)
+		), 
+	      colnames=c("EnsemblID", "uniprot", "Symbol", "Name", "Chr", "idgTDL", "Group", input$score)) %>%
+        formatRound(digits=2, columns=ncol(hits()))
   }, server=T)
-  
-  
+
   hits_export <- reactive({
     if (is.null(hits())) { return(NULL) }
     ggc_hits <- hits()
     ggc_hits["Query"] <- qryA()
     ggc_hits <- ggc_hits[,c(ncol(ggc_hits),1:(ncol(ggc_hits)-1))] #Query col 1st.
-    names(ggc_hits) <- c("Query", "EnsemblID","GeneSymbol","GeneName","Chr", "Group", input$score)
+    names(ggc_hits) <- c("Query", "EnsemblID","UniProt","GeneSymbol","GeneName","Chr", "idgTDL", "Group", input$score)
     return(ggc_hits)
   })
 
   output$hits_file <- downloadHandler(
-    filename = function() { "exfiles_hits.csv" },
+    filename = function() { "exfiles_hits.tsv" },
     content = function(file) {
       if (is.null(hits_export())) { return(NULL) }
-      write.csv(hits_export(), file, row.names=F) 
+      write_delim(hits_export(), file, delim="\t") 
   })
 
   output$plot <- renderPlotly({
@@ -407,7 +444,7 @@ server <- function(input, output, session) {
     }
     
     xaxis = list(tickangle=45, tickfont=list(family="Arial", size=10), categoryorder = "array", categoryarray = tissue$SMTSD)
-    yaxis = list(title=ifelse("LogY" %in% input$plot_opts, "Expression: LOG<SUB>10</SUB>(1+TPM)", "Expression: TPM"))
+    yaxis = list(title=ifelse("LogY" %in% input$opts, "Expression: LOG<SUB>10</SUB>(1+TPM)", "Expression: TPM"))
     
     p <- plot_ly() %>%
       layout(xaxis = xaxis, yaxis = yaxis, 
@@ -417,12 +454,12 @@ server <- function(input, output, session) {
          font = list(family="Arial", size=14)
       )
 
-    if ("SABV" %in% input$plot_opts) {
-      p <-  add_trace(p, name = paste("(F)", qryA()), x = tissue$SMTSD, y = EpLogIf(qryA_profile_f, ("LogY" %in% input$plot_opts)),
+    if ("SABV" %in% input$opts) {
+      p <-  add_trace(p, name = paste("(F)", qryA()), x = tissue$SMTSD, y = EpLogIf(qryA_profile_f, ("LogY" %in% input$opts)),
             type = 'scatter', mode = 'lines+markers',
             marker = list(symbol="circle", size=10),
             text = paste0(qryA(), ": ", tissue$SMTSD))
-      p <- add_trace(p, name = paste("(M)", qryA()), x = tissue$SMTSD, y = EpLogIf(qryA_profile_m, ("LogY" %in% input$plot_opts)),
+      p <- add_trace(p, name = paste("(M)", qryA()), x = tissue$SMTSD, y = EpLogIf(qryA_profile_m, ("LogY" %in% input$opts)),
             type = 'scatter', mode = 'lines+markers',
             marker = list(symbol="circle", size=10),
             text = paste0(qryA(), ": ", tissue$SMTSD))
@@ -432,12 +469,12 @@ server <- function(input, output, session) {
             sprintf("Mab: rho = %.2f; ruz = %.2f", wrhoMab, ruzMab),
             sprintf("Afm: rho = %.2f; ruz = %.2f", wrhoAfm, ruzAfm),
             sprintf("Bfm: rho = %.2f; ruz = %.2f", wrhoBfm, ruzBfm))
-        p <- add_trace(p, name = paste("(F)", qryB()), x = tissue$SMTSD, y = EpLogIf(qryB_profile_f, ("LogY" %in% input$plot_opts)),
+        p <- add_trace(p, name = paste("(F)", qryB()), x = tissue$SMTSD, y = EpLogIf(qryB_profile_f, ("LogY" %in% input$opts)),
             type = 'scatter', mode = 'lines+markers',
             marker = list(symbol="circle", size=10),
             text = paste0(qryB(), ": ", tissue$SMTSD))
         #
-        p <- add_trace(p, name = paste("(M)", qryB()), x = tissue$SMTSD, y = EpLogIf(qryB_profile_m, ("LogY" %in% input$plot_opts)),
+        p <- add_trace(p, name = paste("(M)", qryB()), x = tissue$SMTSD, y = EpLogIf(qryB_profile_m, ("LogY" %in% input$opts)),
             type = 'scatter', mode = 'lines+markers',
             marker = list(symbol="circle", size=10),
             text = paste0(qryB(), ": ", tissue$SMTSD))
@@ -454,11 +491,11 @@ server <- function(input, output, session) {
           for (i in rows_selected) {
             profile_f <- as.numeric(eps[eps$ENSG==hits()$EnsemblID[i] & eps$SEX=="F",][1,tissue$SMTSD])
             profile_m <- as.numeric(eps[eps$ENSG==hits()$EnsemblID[i] & eps$SEX=="M",][1,tissue$SMTSD])
-            p <- add_trace(p, name = paste("(F)", hits()$symbol[i]), x = tissue$SMTSD, y = EpLogIf(profile_f, ("LogY" %in% input$plot_opts)),
+            p <- add_trace(p, name = paste("(F)", hits()$symbol[i]), x = tissue$SMTSD, y = EpLogIf(profile_f, ("LogY" %in% input$opts)),
                 type = 'scatter', mode = 'lines+markers',
                 marker = list(symbol="circle", size=10),
                 text = paste0(hits()$symbol[i], ": ", tissue$SMTSD))
-            p <- add_trace(p, name = paste("(M)", hits()$symbol[i]), x = tissue$SMTSD, y = EpLogIf(profile_m, ("LogY" %in% input$plot_opts)),
+            p <- add_trace(p, name = paste("(M)", hits()$symbol[i]), x = tissue$SMTSD, y = EpLogIf(profile_m, ("LogY" %in% input$opts)),
                 type = 'scatter', mode = 'lines+markers',
                 marker = list(symbol="circle", size=10),
                 text = paste0(hits()$symbol[i], ": ", tissue$SMTSD))
@@ -468,13 +505,13 @@ server <- function(input, output, session) {
         annotxt <- sprintf("Afm: rho = %.2f; ruz = %.2f", wrhoAfm, ruzAfm)
       }
     } else { #NOT_SABV
-      p <-  add_trace(p, name = qryA(), x = tissue$SMTSD, y = EpLogIf(qryA_profile, ("LogY" %in% input$plot_opts)),
+      p <-  add_trace(p, name = qryA(), x = tissue$SMTSD, y = EpLogIf(qryA_profile, ("LogY" %in% input$opts)),
             type = 'scatter', mode = 'lines+markers',
             marker = list(symbol="circle", size=10),
             text = paste0(qryA(), ": ", tissue$SMTSD))
       if (input$mode=="Compare" & !is.null(qryB())) {
         annotxt <- sprintf("rho = %.2f; ruz = %.2f", wrho, ruz)
-        p <- add_trace(p, name = qryB(), x = tissue$SMTSD, y = EpLogIf(qryB_profile, ("LogY" %in% input$plot_opts)),
+        p <- add_trace(p, name = qryB(), x = tissue$SMTSD, y = EpLogIf(qryB_profile, ("LogY" %in% input$opts)),
             type = 'scatter', mode = 'lines+markers',
             marker = list(symbol="circle", size=10),
             text = paste0(qryB(), ": ", tissue$SMTSD))
@@ -488,7 +525,7 @@ server <- function(input, output, session) {
             profile_f <- as.numeric(eps[eps$ENSG==hits()$EnsemblID[i] & eps$SEX=="F",][1,tissue$SMTSD])
             profile_m <- as.numeric(eps[eps$ENSG==hits()$EnsemblID[i] & eps$SEX=="M",][1,tissue$SMTSD])
             profile <- (profile_f + profile_m)/2
-            p <- add_trace(p, name = hits()$symbol[i], x = tissue$SMTSD, y = EpLogIf(profile, ("LogY" %in% input$plot_opts)),
+            p <- add_trace(p, name = hits()$symbol[i], x = tissue$SMTSD, y = EpLogIf(profile, ("LogY" %in% input$opts)),
                 type = 'scatter', mode = 'lines+markers',
                 marker = list(symbol="circle", size=10),
                 text = paste0(hits()$symbol[i], ": ", tissue$SMTSD))
@@ -499,7 +536,7 @@ server <- function(input, output, session) {
       }
     }
     #
-    if ("Showstats" %in% input$plot_opts) {
+    if ("Annplot" %in% input$opts) {
       p <- add_annotations(p, text=annotxt, showarrow=F, x=.1, y=1, xref="paper", yref="paper")
     }
     p$elementId <- NULL #Hack to suppress spurious warnings.
