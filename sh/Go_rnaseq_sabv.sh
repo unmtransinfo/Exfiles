806188c55603dcbb9589c6a15b2152748c6f95b1
Jeremy J Yang <jeremyjyang@gmail.com>
2019-07-11 08:59:13 -0600

    Ok.

--- sh/Go_rnaseq_sabv.sh
+++ sh/Go_rnaseq_sabv.sh
@@ -9,10 +9,14 @@ printf "Start: %s\n" "$(date)"
 #
 set -x
 #
+#
+# Samples file required for Wilcoxon rank sum statistic.
+# This may be very slow on full file. 24h+?
+#	--i_sample $DATADIR/gtex_rnaseq_prep_sample.tsv \
+#
 $cwd/python/gtex_rnaseq_sabv.py \
 	--i $DATADIR/gtex_rnaseq_prep_median.tsv \
-	--i_sample $DATADIR/gtex_rnaseq_prep_sample.tsv \
-	--o $DATADIR/gtex_rnaseq_sabv.tsv \
+	--o $DATADIR/gtex_rnaseq_sabv_no-Wilcoxon.tsv \
 	-v
 #
 printf "Done: %s\n" "$(date)"
