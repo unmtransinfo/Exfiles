32fc4e9050c84235a204455cfc5a6969a19ae288
Jeremy Yang <jeremyjyang@gmail.com>
2021-06-25 14:29:14 -0600

    Ok.

--- python/h5_app.py
+++ python/h5_app.py
@@ -3,7 +3,17 @@
 
 import sys,os,argparse,logging
 import numpy as np
+import pandas as pd
 import h5py
+
+def h5_type(ob):
+  return "Group" if type(ob) is h5py.Group else "Dataset" if type(ob) is h5py.Dataset else type(ob)
+
+def list_all(name, ob):
+  logging.info(f"{ob.name} ({h5_type(ob)})")
+  if type(ob) is h5py.Dataset:
+    logging.info(f"Dataset dtype: {ob.dtype}; shape: {ob.shape}; size: {ob.size}; ndim: {ob.ndim}; nbytes: {ob.nbytes}")
+  return None
 
 if __name__=='__main__':
   parser = argparse.ArgumentParser(description='H5 file operations', epilog="")
@@ -20,10 +30,22 @@ if __name__=='__main__':
 
   logging.debug(list(f.keys()))
 
-  for key in list(f.keys()):
-    dset = f[key]
-    logging.debug(f"type(f[{key}]): {type(f[key])}")
-    #logging.debug(f"Dataset {key} type: {dset.dtype}")
-    #logging.debug(f"Dataset {key} shape: {dset.shape}")
+  for k in list(f.keys()):
+    if type(f[k]) is h5py.Group:
+      logging.debug(f"{f.name}:{f[k].name}")
+      f[k].visititems(list_all)
+    else:
+      logging.info(f"{f[k].name} ({h5_type(f[k])})")
 
 
+  samples_title = f["meta"]["samples/title"]
+  print(pd.DataFrame(samples_title.asstr()[0:10,]))
+
+  samples = f["meta"]["samples"]
+  df = pd.DataFrame()
+  for k in list(samples.keys()):
+    if type(samples[k]) is h5py.Dataset:
+      df_this = pd.DataFrame(samples[k])
+      df = pd.concat([df, df_this], axis=1)
+
+  print(df[0:10,])
