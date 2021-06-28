9ebd30f451f7e21d57ffa7c0f260f0a1386aeafc
Jeremy Yang <jeremyjyang@gmail.com>
2021-06-28 13:53:22 -0600

    Ok.

--- python/h5_app.py
+++ python/h5_app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 ###
 
-import sys,os,argparse,logging
+import sys,os,time,argparse,logging
 import numpy as np
 import pandas as pd
 import h5py
@@ -16,11 +16,12 @@ def list_all(name, ob):
   return None
 
 #############################################################################
-def ListSamples(f):
-
-  samples_title = f["meta"]["samples/title"]
-  print(pd.DataFrame(samples_title.asstr()[0:10,]))
-
+def ListSamples(f, fout):
+  if "meta" not in f or "samples" not in f["meta"]:
+    logging.error("No samples found.")
+    return
+  #samples_title = f["meta"]["samples/title"]
+  #print(pd.DataFrame(samples_title.asstr()[0:10,]))
   samples = f["meta"]["samples"]
   df = pd.DataFrame()
   for i,k in enumerate(list(samples.keys())):
@@ -28,15 +29,32 @@ def ListSamples(f):
     if type(samples[k]) is h5py.Dataset:
       df_this = pd.DataFrame(samples[k])
       df = pd.concat([df, df_this], axis=1)
-    if i>2: break #DEBUG
+      df = df.drop_duplicates()
+    #if i>2: break #DEBUG
+  df.columns = list(samples.keys())
+  #print(df.iloc[0:10,:])
 
-  print(df.iloc[0:10,:])
+  for col, dtype in df.dtypes.items():
+    if dtype == np.object:  # Only process object columns.
+      # decode, or return original value if decode return Nan
+      df[col] = df[col].str.decode('utf-8').fillna(df[col]) 
+
+  logging.info(f"Output rows: {df.shape[0]}; columns: {df.shape[1]}")
   df.to_csv(fout, "\t", index=False)
+
+#############################################################################
+def Summary(f):
+  for k in list(f.keys()):
+    if type(f[k]) is h5py.Group:
+      logging.debug(f"{f.name}:{f[k].name}")
+      f[k].visititems(list_all)
+    else:
+      logging.info(f"{f[k].name} ({h5_type(f[k])})")
 
 #############################################################################
 if __name__=='__main__':
   parser = argparse.ArgumentParser(description='H5 file operations', epilog="")
-  OPS = ['summary']
+  OPS = ['summary', 'list_samples']
   parser.add_argument("op", choices=OPS, help='OPERATION')
   parser.add_argument("--i", dest="ifile", required=True, help="input file")
   parser.add_argument("--o", dest="ofile", help="output (TSV)")
@@ -45,21 +63,21 @@ if __name__=='__main__':
 
   logging.basicConfig(format='%(levelname)s:%(message)s', level=(logging.DEBUG if args.verbose>1 else logging.INFO))
 
+  t0 = time.time();
+
   fout = open(args.ofile, "w") if args.ofile else sys.stdout
 
-  f = h5py.File(args.ifile, 'r')
+  finh5 = h5py.File(args.ifile, 'r')
 
-  logging.debug(list(f.keys()))
+  if args.op == "summary":
+    logging.debug(list(finh5.keys()))
+    Summary(finh5)
 
-  for k in list(f.keys()):
-    if type(f[k]) is h5py.Group:
-      logging.debug(f"{f.name}:{f[k].name}")
-      f[k].visititems(list_all)
-    else:
-      logging.info(f"{f[k].name} ({h5_type(f[k])})")
+  elif args.op == "list_samples":
+    ListSamples(finh5, fout)
 
+  else:
+    parser.error(f"Unsupported operation: {args.op}")
 
-  if "meta" not in f or "samples" not in f["meta"]:
-    sys.exit(1)
+  logging.info(f"Elapsed: {time.strftime('%Hh:%Mm:%Ss', time.gmtime(time.time()-t0))}")
 
-  ListSamples(f)
