3e440d6db71cec4dca081e69cdaea8393ed4c787
Jeremy Yang <jeremyjyang@gmail.com>
2022-06-17 15:37:41 -0600

    Ok.

--- sh/Go_biomart_GetData.sh
+++ sh/Go_biomart_GetData.sh
@@ -3,6 +3,13 @@
 # https://m.ensembl.org/info/data/biomart/biomart_restful.html#biomartperlapi
 # https://m.ensembl.org/info/data/biomart/biomart_restful.html#biomartxml
 
+###
+# See also:
+# http://uswest.ensembl.org/info/data/biomart/biomart_restful.html#wget
+# E.g.
+# wget -O result.tsv "http://www.ensembl.org/biomart/martservice?query=$(cat biomart_ENSG2NCBI_query.xml)"
+###
+
 #
 cwd=$(pwd)
 DATADIR=${cwd}/data
@@ -11,3 +18,4 @@ perl $HOME/../app/biomart-perl/scripts/webExample.pl \
 	${cwd}/sh/biomart_ENSG2NCBI_query.xml \
 	>$DATADIR/biomart_ENSG2NCBI_human.tsv
 #
+
